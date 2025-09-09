2cad611338af39c1a7488095148fc488d69e989e
Aditya Magdum <adityamagdum555@gmail.com>
2025-09-09 11:17:23 +0530

    user_schema.py

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -6,11 +6,15 @@ class UserCreate(BaseModel):
     email: EmailStr
     password: str
 
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
 class UserOut(BaseModel):
     id: int
     name: str
     email: EmailStr
-    created_at: Optional[str]
+    created_at: Optional[str] = None
 
     class Config:
         orm_mode = True
